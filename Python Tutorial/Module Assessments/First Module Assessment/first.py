c1bb41c62799dd28082c981fd52a5655e8e4fcf7
ranaMETI <rana.gamal@meti.dev>
2024-12-26 16:54:05 +0200

    First Commit

---
+++ Python Tutorial/Module Assessments/First Module Assessment/first.py
@@ -0,0 +1,11 @@
+# First Python Program - Introduction
+
+# Personal details
+name = "Rana"  
+age = 23  
+goal = "Learn Python programming and build interactive projects."
+
+# Introduction
+print("Hello, my name is", name)
+print("I am", age, "years old.")
+print("In this project, I aim to:", goal)
